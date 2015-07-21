66f82e84bfd5970839a0862521c8c5522d68744a
connor <connor@glockenspiel.chime>
2015-07-21 12:39:02 -0700

    My concerns about normalizing by packet loss were for nothing. Andata doesn't normalize them either

--- vdif_script.py
+++ vdif_script.py
@@ -17,6 +17,8 @@ f_dir = '/drives/0/baseband/20150717T172614Z_chime_beamformed/'
 f_dir = '/drives/0/baseband/20150717T194826Z_chime_beamformed/'
 f_dir = '/drives/0/baseband/20150718T154633Z_chime_beamformed/' # One pol has ~10 beams on the same cylinder
 
+outfile = '/drives/0/liamscratch/b0329+54_10feeds.hdf5'
+
 flist = glob.glob(f_dir + '/*dat')
 flist.sort()
 
@@ -28,9 +30,9 @@ print "Frame range:"
 print "------------"
 
 
-RB = rbf.ReadBeamform(pmax=1e6)
+RB = rbf.ReadBeamform(pmax=1e8)
 
-for ii in range(0, 100, 1):
+for ii in range(0, 100, 20):
      print "reading %s" % flist[ii]
 
      if os.path.isfile(flist[ii]) is False:
@@ -45,13 +47,13 @@ for ii in range(0, 100, 1):
      h, d = read_arrs
      v, tt = RB.h_index(d, h, trb=ntrb)
 
-     trb = 10
+     trb = 1
 
-     v = v[:(len(v)//trb)*trb].reshape(-1, trb, 2, 1024)
-     nonz = np.where(v==0, 0, 1).sum(1)
+     #v = v[:(len(v)//trb)*trb].reshape(-1, trb, 2, 1024)
+     #nonz = np.where(v==0, 0, 1).sum(1)
 
-     #arr.append(v[:(len(v)//trb)*trb].reshape(-1, trb, 2, 1024).mean(1))
-     arr.append(v.sum(1) / nonz)
+     arr.append(v[:(len(v)//trb)*trb].reshape(-1, trb, 2, 1024).mean(1))
+     #arr.append(v.sum(1) / nonz)
      
      del v, d
      
@@ -60,16 +62,13 @@ for ii in range(0, 100, 1):
      del h
 
 arr = np.concatenate(arr)
-tt_tot = np.concatenate(tt_tot, axis=0)
+tt_tot = (np.concatenate(tt_tot, axis=0))[::trb]
 #t_tot = RB.get_times(h_tot)
 
 print "here"
 
-outfile = '/drives/0/liamscratch/b0329+54_10feeds.hdf5'
-
 f = h5py.File(outfile, 'w')
 f.create_dataset('arr', data=arr)
-#f.create_dataset('times', data=t_tot)
 f.create_dataset('times', data=tt_tot)
 f.close()
 
