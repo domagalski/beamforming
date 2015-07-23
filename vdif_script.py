4c76ca411627aa446972d3461ccedff6b7a105c2
connor <connor@glockenspiel.chime>
2015-07-23 05:52:54 -0700

    made more general

--- vdif_script.py
+++ vdif_script.py
@@ -7,17 +7,25 @@ import glob
 import ReadBeamform as rbf
 
 trb = 1 # Factor to rebin data array by
-nfiles = 10 # Total number of files to use
-f_step = 1 # step between files
+nfiles = 10000 # Total number of files to use
+f_start = 0
+f_step = 50 # step between files
 
 f_dir = '/drives/0/baseband/20150710T195202Z_chime_beamformed/'
 f_dir = '/drives/0/baseband/20150711T160938Z_chime_beamformed/'
 f_dir = '/drives/0/baseband/20150716T194720Z_chime_beamformed/' # Solar both pol one feed
-f_dir = '/drives/0/baseband/20150717T172614Z_chime_beamformed/'
+#f_dir = '/drives/0/baseband/20150717T172614Z_chime_beamformed/'
 f_dir = '/drives/0/baseband/20150717T194826Z_chime_beamformed/'
-f_dir = '/drives/0/baseband/20150718T154633Z_chime_beamformed/' # One pol has ~10 beams on the same cylinder
+#f_dir = '/drives/0/baseband/20150718T154633Z_chime_beamformed/' # One pol b0329+54 has ~10 beams on the same cylinder
+#f_dir = '/drives/0/baseband//20150722T144418Z_chime_beamformed/' # Synthetic data from Andre
+#f_dir = '/drives/0/baseband//20150722T144820Z_chime_beamformed/'
+#f_dir = '/drives/0/baseband//20150722T150516Z_chime_beamformed/'
+f_dir = '/drives/0/baseband/20150722T151521Z_chime_beamformed/' # B0329+54 multiple 
+#f_dir = '/drives/0/baseband//20150722T211314Z_chime_beamformed/'
+#f_dir = '/drives/0/baseband//20150722T221328Z_chime_beamformed/'
+f_dir = '/drives/0/baseband//20150723T002219Z_chime_beamformed/' # Virgo A
 
-outfile = '/drives/0/liamscratch/b0329+54_10feeds.hdf5'
+outfile = '/drives/0/liamscratch/virA.hdf5'
 
 flist = glob.glob(f_dir + '/*dat')
 flist.sort()
@@ -33,7 +41,7 @@ print "------------"
 
 RB = rbf.ReadBeamform(pmax=1e8)
 
-for ii in range(0, nfiles, f_step):
+for ii in range(f_start, f_start + nfiles, f_step):
      print "reading %s" % flist[ii]
 
      if os.path.isfile(flist[ii]) is False:
