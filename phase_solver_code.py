ae46d6ff18554b159659830e818349c6851afa7b
connor <connor@marimba.(none)>
2015-08-30 17:35:06 +0000

    New way of comparing beamformed andata

--- phase_solver_code.py
+++ phase_solver_code.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import datetime
 import h5py
@@ -14,6 +16,8 @@ def plt_gains(vis, nu, img_name='out.png'):
     
     for i in range(128):
         fig.add_subplot(32, 4, i+1)
+        if i==1:
+            plt.plot(vis[nu, misc.feed_map(1, i+1, 128)])
         plt.plot(np.angle(vis[nu, misc.feed_map(1, i+1, 128)]))
         plt.axis('off')
         plt.axhline(0.0)
@@ -88,40 +92,15 @@ def read_data(reader_obj, src, prod_sel, freq_sel=None, del_t=50):
 
     return and_obj
 
-def run_gain_solver(freq_range, xcorrs, ycorrs, inpx, inpy):
-
-    Xy = read_data(R, src, ycorrs, freq_sel=freq_range, del_t=1000)   
-    Xx = read_data(R, src, xcorrs, freq_sel=freq_range, del_t=1000)
-
-    datay = Xy.vis[..., ::2]
-    datay -= 0.5 * (datay[..., 0] + datay[..., -1])[..., np.newaxis] 
-
-    datax = Xx.vis[..., ::2]
-    datax -= 0.5 * (datax[..., 0] + datax[..., -1])[..., np.newaxis] 
-
-    data_fs_x = tools.fringestop_pathfinder(\
-         datax, eph.transit_RA(Xx.timestamp[::2]), Xx.freq, inpx, src)
-
-    del Xx
-    
-    data_fs_y = tools.fringestop_pathfinder(\
-         datay, eph.transit_RA(Xy.timestamp[::2]), Xy.freq, inpy, src)
-
-    del Xy
-
-    dx, ax = solve_gain(data_fs_x)
-    dy, ay = solve_gain(data_fs_y)
-
-    del data_fs_x, data_fs_y
-
-    return ax, dx, ay, dy
-
-def solve_untrans(filename, corrs, inp, src, nfreq=1024):
-    del_t = 400
+def solve_untrans(filename, corrs, inp, src, nfreq=1024, transposed=False):
+    del_t = 200
 
     f = h5py.File(filename, 'r')
 
     times = f['index_map']['time'].value['ctime']
+    
+    # Adjust for cylinder rotation. Things should arrive ~half a degree late. 
+    src._ra += np.radians(0.35)
 
     src_trans = eph.transit_times(src, times[0])
 
@@ -134,25 +113,39 @@ def solve_untrans(filename, corrs, inp, src, nfreq=1024):
 
     Gains = np.zeros([nfreq, 128, len(times)], np.complex128)
     
-    print "Reading full array"
+    print "Starting the solver"
     
-    v = f['vis'][t_range[0]:t_range[-1]:2, :, corrs]
+    nsplit = 32
 
-    print "Done reading array"
-
-    for i in range(16):
-
-        frq = range(i * nfreq // 16, (i+1) * nfreq // 16)
+    for i in range(nsplit):
+        
+        ## Divides the arrays up into nfreq / nsplit freq chunks and solves those
+        frq = range(i * nfreq // nsplit, (i+1) * nfreq // nsplit)
 
         print "      ", frq[0], ":", frq[-1]
 
-        vis = v['r'][:, frq] + 1j * v['i'][:, frq]
-        vis = np.transpose(vis, (1, 2, 0))
+        if transposed is True:
+            v = f['vis'][frq[0]:frq[-1]+1, corrs, :]
+            v = v[..., t_range[0]:t_range[-1]:2]
+            vis = v['r'] + 1j * v['i']
+            del v
+            print "Transie"
+
+        if transposed is False:
+            v = f['vis'][t_range[0]:t_range[-1]:2, frq[0]:frq[-1]+1, corrs]
+            vis = v['r'][:] + 1j * v['i'][:]
+            del v
+            vis = np.transpose(vis, (1, 2, 0))
+            print "Untransie"
+
         vis -= 0.5 * (vis[..., 0] + vis[..., -1])[..., np.newaxis]
         
         freq_MHZ = 800.0 - np.array(frq) / 1024.0 * 400.
         
         data_fs = tools.fringestop_pathfinder(vis, eph.transit_RA(times), freq_MHZ, inp, src)
+
+        del vis
+
         dr, a = solve_gain(data_fs)
 
         Gains[frq] = a 
@@ -160,9 +153,8 @@ def solve_untrans(filename, corrs, inp, src, nfreq=1024):
         plt_gains(data_fs, 0, img_name='./phs_plots/dfs' + np.str(frq[0]) + '.png')
         dfs_corr = correct_dfs(data_fs, Gains[frq], nfeed=128)
         plt_gains(dfs_corr, 0, img_name='./phs_plots/dfs_corr' + np.str(frq[0]) + '.png')
-        
 
-    del v, vis, data_fs
+        del data_fs, a, dfs_corr
 
     return Gains
 
@@ -251,46 +243,126 @@ def gen_inp(nfeed=256):
 
     return corrinput_real, inpx, inpy, xcorrs, ycorrs
 
-def select_corrs(data, feeds, nfeed=256):
+def select_corrs(feeds, nfeed=256):
+    autos = []
     corrs = []
     
     for ii in range(len(feeds)):
         for jj in range(ii, len(feeds)):
-            corrs.append(misc.feed_map(feeds[ii], feeds[jj], nfeed))
+            if ii==jj:
+                autos.append(misc.feed_map(feeds[ii], feeds[jj], nfeed))
+            else:
+                corrs.append(misc.feed_map(feeds[ii], feeds[jj], nfeed))
     
-    return data[:, corrs]
+    return autos, corrs
 
-def fringestop_do_it_all(fn, feeds, freq, src, return_unfs=False):                                                 
-    r = andata.Reader(fn)
-    r.freq_sel = freq
-    X = r.read()
-    times = r.time[::2]
+def sum_corrs(data, feeds):
+    autos, corrs = select_corrs(feeds)
+    
+    return data[:, autos].sum(1) + 2 * data[:, corrs].sum(1)
+
+def fringestop_pathfinder(timestream, ra, freq, feeds, src):
+
+    import scipy.constants
+    ephemeris = eph
+
+    # Calculate the hour angle                                                                                                                       \
+                                                                                                                                                      
+    ha = (np.radians(ra) - src._ra)[np.newaxis, np.newaxis, :]
+
+    _PF_LAT = np.radians(ephemeris.CHIMELATITUDE)   # Latitude of pathfinder                                                                         \
+                                                                                                                                                      
+
+    # Get feed positions                                                                                                                             \
+                                                                                                                                                      
+    feedpos = tools.get_feed_positions(feeds)
+    xp, yp = feedpos[:, 0], feedpos[:, 1]
+
+    # Calculate baseline separations and pack into product array                                                                                     \
+                                                                                                                                                      
+    xd = xp[:, np.newaxis] - xp[np.newaxis, :]
+    yd = yp[:, np.newaxis] - yp[np.newaxis, :]
+    xd = tools.pack_product_array(xd, axis=0)
+    yd = tools.pack_product_array(yd, axis=0)
+
+    # Calculate wavelengths and UV place separations                                                                                                 \
+                                                                                                                                                      
+    wv = scipy.constants.c * 1e-6 / freq[:, np.newaxis, np.newaxis]
+    u = xd[np.newaxis, :, np.newaxis] / wv
+    v = yd[np.newaxis, :, np.newaxis] / wv
+    print np.round(u[0, 256:512, 0], 2)
+    print np.round(v[0, 256:512, 0], 2)
+
+    # Construct fringestop phase and set any non CHIME feeds to have zero phase                                                                      \
+                                                                                                                                                      
+    fs_phase = tools.fringestop_phase(ha, _PF_LAT, src._dec, u, v)
+    fs_phase = np.where(np.isnan(fs_phase), np.zeros_like(fs_phase), fs_phase)
+
+    return timestream * fs_phase
+
+
+def fringestop_and_sum(fn, feeds, freq, src, transposed=True, return_unfs=False, meridian=False, del_t=750):             
+    """ Take an input file fn and a set of feeds and return 
+    a formed beam on src. 
+    """
+    if transposed is True:
+        r = andata.Reader(fn)
+        r.freq_sel = freq
+        X = r.read()
+        times = r.time
+    else:
+        f = h5py.File(fn, 'r')  
+        times = f['index_map']['time'].value['ctime']
 
     print "Read it, bruh"
 
-    del_t = 750.0
+    # Get transit time for source 
     src_trans = eph.transit_times(src, times[0])
 
-    # Select +-100 seconds of transit                                                                                                                                       
+    # Select +-100 seconds of transit                                                                                                                    
     t_range = np.where((times < src_trans + del_t) & (times > src_trans - del_t))[0]
 
+    # Generate correctly ordered corrinputs
     corrinput_real = gen_inp()[0]
-    inp = np.array(corrinput_real)[feeds]
+    inp = np.array(corrinput_real)
 
-    data = X.vis[..., ::2]
-    data -= (data[..., t_range[0]] + data[..., t_range[-1]])[..., np.newaxis] / 2.0
-    print t_range[0], t_range[-1]
+    # Ensure vis array is in correct order (freq, prod, time)
+    if transposed is True:
+        data = X.vis[:, :, t_range[0]:t_range[-1]]
+        freq = X.freq
+    else:
+        v = f['vis'][t_range[0]:t_range[-1], freq, :] 
+        vis = v['r'] + 1j * v['i']
+        data = vis.transpose()[np.newaxis]
 
-    data = select_corrs(data, feeds)
+        del vis
 
-    ra = eph.transit_RA(times)
+        freq = 800 - 400.0 / 1024 * freq
+        freq = np.array([freq])
 
-    dfs = tools.fringestop_pathfinder(data, ra, X.freq, inp, src)
+    times = times[t_range[0]:t_range[-1]]
+#    data -= (data[..., 0] + data[..., -1])[..., np.newaxis] / 2.0
+
+    print "Time range:", t_range[0], t_range[-1]
+
+    data_unfs = sum_corrs(data.copy(), feeds)
+    ra_ = eph.transit_RA(times)
+
+    if meridian is True:
+        ra = np.ones_like(ra_) * np.degrees(src._ra)
+    else:
+        ra = ra_
+
+    dfs = tools.fringestop_pathfinder(data.copy(), ra, freq, inp, src)    
+#    dfs2 = fringestop_pathfinder(data.copy(), ra, freq, inp, src)
+
+    dfs_sum = sum_corrs(dfs, feeds)
 
     if return_unfs is True:
-        return dfs, ra, data
+        return dfs_sum, ra_, dfs, data_unfs
     else:
-        return dfs, ra
+        return dfs_sum, ra_
+
 
 flist = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 
          60, 61, 62, 63, 16, 17, 18, 19, 20, 21, 22, 23, 
