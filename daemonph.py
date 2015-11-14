7977dd3b50b6e4da1ab126b0a4bb21a588a6cb36
Liam Connor <liam.dean.connor@gmail.com>
2015-11-14 09:52:58 -0500

    New daemon to run calibation

--- daemonph.py
+++ daemonph.py
@@ -9,7 +9,13 @@ time_yesterday = time.time() - 24 * 3600.0
 
 src = CasA
 
-# Find previous day's transit file for CasA
-fn = pc.find_transit(time_yesterday)
+# Run script daily
+sleep_time = 24 * 3600.0
 
-os.system('python ph_solver_script.py %s %s' % (fn, src))
+while True:
+     # Find previous day's transit file for CasA
+     fn = pc.find_transit(time_yesterday)
+
+     os.system('python ph_solver_script.py %s %s' % (fn, src))
+
+     time.sleep(sleep_time)
